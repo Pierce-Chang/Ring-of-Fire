0244127c1afd4244e40bd1425e68988f10a45091
Pierce Chang <143874526+Pierce-Chang@users.noreply.github.com>
2023-12-19 12:04:45 +0100

    first playable commit

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -62,7 +62,7 @@ export class GameComponent implements OnInit {
     const dialogRef = this.dialog.open(DialoagAddPlayerComponent);
 
     dialogRef.afterClosed().subscribe((name: string) => {
-      if (name.length > 0) {
+      if (name && name.length > 0) {
         this.game.players.push(name);
       }
     });
